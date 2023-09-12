5df90d921844ad9b1298bdab52125245843a3fed
Vishvanath1311 <144792166+Vishvanath1311@users.noreply.github.com>
2023-09-12 03:40:39 -0700

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -7,7 +7,10 @@ int main()
 	cout<<"Enter the 5 marks: ";
 	cin>>a>>b>>c>>d>>e;
 	float avg;
-	avg=(float)(a+b+c+d+e)/5;
+	int sum;
+	sum=a+b+c+d+e;
+	avg=(float)sum/5;
+	//avg=(float)(a+b+c+d+e)/5;
 	cout<<"Average of 5 marks is: "<<avg<<endl;
 	return 0;
 }
