7edaae3eb0d90faae7423a5a9203e558ccbd625c
01Bhavesh <144714226+01Bhavesh@users.noreply.github.com>
2023-09-12 16:13:45 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main()
 {
 	int r;
-	const float pi=3.14f;
+	const float pi1=3.14f;
 	cout<<"Enter the radius: "<<endl;
 	cin>>r;
 	float A, cir;
-	A=pi*r*r;
-	cir=2*pi*r;
+	A=pi1*r*r;
+	cir=2*pi1*r;
 	cout<<"Area of circle: "<<A<<'\n'<<"Circumferance of circle: "<<cir<<endl;
 	
 }
